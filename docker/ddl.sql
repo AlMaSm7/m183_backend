8376980edc92f77812035c45c05267e61030aa9c
Aaron Holenstein <4aron.dodger@gmail.com>
2023-06-22 14:01:35 +0200

    DB Adjust - removed Folders

--- docker/ddl.sql
+++ docker/ddl.sql
@@ -7,18 +7,12 @@ CREATE TABLE `User` (
 ) ENGINE = InnoDB;
 CREATE TABLE `Record` (
     `id` INT NOT NULL AUTO_INCREMENT ,
+    `user_id` INT NOT NULL ,
     `name` VARCHAR(255) NOT NULL ,
     `username` VARCHAR(255) NULL DEFAULT NULL ,
     `password` VARCHAR(255) NOT NULL ,
     `url` VARCHAR(255) NULL DEFAULT NULL ,
     `note` TEXT NULL DEFAULT NULL ,
-    `folder_id` INT(11) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
-CREATE TABLE `Folder` (
-    `id` INT NOT NULL AUTO_INCREMENT ,
-    `name` VARCHAR(255) NOT NULL ,
-    `parent_folder` INT NULL ,
-    `user_id` INT NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES User(`id`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = InnoDB;
