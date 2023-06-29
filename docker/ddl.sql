882fa5ceb89a098114e3bda0a38c4897783d383c
alex_m_s <alex_m_s@yahoo.com>
2023-06-29 13:58:38 +0200

    added new fields and naming

--- docker/ddl.sql
+++ docker/ddl.sql
@@ -1,18 +1,20 @@
-CREATE TABLE `User` (
-    `id` INT NOT NULL AUTO_INCREMENT ,
-    `username` VARCHAR(255) NOT NULL ,
-    `email` VARCHAR(255) NOT NULL ,
-    `password` VARCHAR(255) NOT NULL ,
-    PRIMARY KEY (`id`)
+CREATE TABLE `passwordManager`.`user` (
+    `user_id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL UNIQUE,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `salt`  VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`user_id`)
 ) ENGINE = InnoDB;
-CREATE TABLE `Record` (
-    `id` INT NOT NULL AUTO_INCREMENT ,
-    `user_id` INT NOT NULL ,
-    `name` VARCHAR(255) NOT NULL ,
-    `username` VARCHAR(255) NULL DEFAULT NULL ,
-    `password` VARCHAR(255) NOT NULL ,
-    `url` VARCHAR(255) NULL DEFAULT NULL ,
-    `note` TEXT NULL DEFAULT NULL ,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`user_id`) REFERENCES User(`id`) ON UPDATE CASCADE ON DELETE CASCADE
+
+CREATE TABLE `passwordManager`.`record` (
+    `record_id` INT NOT NULL AUTO_INCREMENT,
+    `user_id_fk` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NULL DEFAULT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `url` VARCHAR(255) NULL DEFAULT NULL,
+    `note` TEXT NULL DEFAULT NULL,
+    PRIMARY KEY (`record_id`),
+    FOREIGN KEY (`user_id_fk`) REFERENCES `user`(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = InnoDB;
