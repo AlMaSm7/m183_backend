9e1381c670dfb5c8407b88135db29f32b0e59547
alex_m_s <alex_m_s@yahoo.com>
2023-07-06 09:31:30 +0200

    correct user id sql syntax error

--- docker/ddl.sql
+++ docker/ddl.sql
@@ -4,7 +4,7 @@ CREATE TABLE `passwordManager`.`user` (
     `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `salt`  VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`user_id`)
 ) ENGINE = InnoDB;
 CREATE TABLE `passwordManager`.`record` (
     `record_id` INT NOT NULL AUTO_INCREMENT,
@@ -15,5 +15,5 @@ CREATE TABLE `passwordManager`.`record` (
     `url` VARCHAR(255) NULL DEFAULT NULL,
     `note` TEXT NULL DEFAULT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`user_id_fk`) REFERENCES `user`(`id`) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (`user_id_fk`) REFERENCES `user`(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = InnoDB;
